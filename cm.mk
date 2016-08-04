bc4a45d83f58d53b88036fe5966b82864ff3a7ba
Sagar Shah <sagarshah2326@gmail.com>
2016-08-04 22:42:39 +0530

    Update cm.mk

--- cm.mk
+++ cm.mk
@@ -14,13 +14,13 @@
 
 $(call inherit-product, device/yu/tomato/full_tomato.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common DU stuff.
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 # Must define platform variant before including any common things
 TARGET_BOARD_PLATFORM_VARIANT := msm8939
 
-PRODUCT_NAME := cm_tomato
+PRODUCT_NAME := du_tomato
 BOARD_VENDOR := yu
 PRODUCT_DEVICE := tomato
 
